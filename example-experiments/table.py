86c77720381e28fa21b4f46c64cbbeede5b3b3ea
turaco-code <122475098+turaco-code@users.noreply.github.com>
2023-04-04 10:27:57 -0400

    update

--- example-experiments/table.py
+++ example-experiments/table.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
+import driver
 import sys
 import subprocess
 import numpy as np
 
 benchmarks = [
-    ('codes/quake_phi_phi.n', 'Quake'),
-    ('codes/camera.n', 'Camera'),
     ('codes/huber.n', 'Huber'),
     ('codes/axbench/blackscholes-2.n', 'BlackScholes'),
+    ('codes/camera.n', 'Camera'),
+    ('codes/quake_phi_phi.n', 'Quake'),
     ('codes/axbench/jmeint.n', 'Jmeint'),
 ]
 
@@ -20,36 +21,45 @@ def main():
             if string in line:
                 return float(line.split()[-1])
 
-    print('Benchmark & Frequency & Frequency (theoretical) & Uniform & Uniform (theoretical) \\\\')
+    print(r'\toprule')
+    print(r'Benchmark & Lines of Code & Paths & Frequency (Predicted) & Frequency (Measured) & Uniform (Predicted) & Uniform (Measured) \\ \midrule')
 
     for (code, name) in benchmarks:
+        with open(code, 'r') as f:
+            lines_of_code = len(f.readlines())
+
         output = subprocess.check_output([sys.executable, 'plot.py', '--program', code, '--no-theo', '--no-show'], stderr=subprocess.DEVNULL, universal_newlines=True)
+
         freq_empirical = parse_line(output, 'improvement over frequency:')
+        freq_empirical = 1/(1 + freq_empirical)  - 1
+
         freq_theoretical = parse_line(output, 'improvement over frequency (theoretical):')
+        freq_theoretical = 1/(1 + freq_theoretical)  - 1
+
         uniform_empirical = parse_line(output, 'improvement over uniform:')
+        uniform_empirical = 1/(1 + uniform_empirical)  - 1
+
         uniform_theoretical = parse_line(output, 'improvement over uniform (theoretical):')
+        uniform_theoretical = 1/(1 + uniform_theoretical)  - 1
 
-        data[name] = (freq_empirical, freq_theoretical, uniform_empirical, uniform_theoretical)
+        p = driver.read_program(code)
+        distribution = p.config.distribution
 
-        print('{} & {:.2%} & {:.2%} & {:.2%} & {:.2%} \\\\'.format(name, freq_empirical, freq_theoretical, uniform_empirical, uniform_theoretical))
+        data[name] = (freq_empirical, freq_theoretical, uniform_empirical, uniform_theoretical, distribution)
 
-    print(r'\hline')
-    # Euclidean Mean
-    print('Average (euclidean) & {:.2%} & {:.2%} & {:.2%} & {:.2%} \\\\'.format(
-        sum([data[name][0] for name in data]) / len(data),
-        sum([data[name][1] for name in data]) / len(data),
-        sum([data[name][2] for name in data]) / len(data),
-        sum([data[name][3] for name in data]) / len(data),
-    ))
+        print('{} & {} & {} & {:+.2%} & {:+.2%} & {:+.2%} & {:+.2%} \\\\'.format(name, lines_of_code, len(distribution), freq_theoretical, freq_empirical, uniform_theoretical, uniform_empirical))
+
+    print(r'\midrule')
 
     # Geo Mean
-    print('Average (geometric) & {:.2%} & {:.2%} & {:.2%} & {:.2%} \\\\'.format(
-        np.exp(np.mean([np.log(1 + data[name][0]) for name in data])) - 1,
+    print('Geomean & & & {:.2%} & {:.2%} & {:.2%} & {:.2%} \\\\'.format(
         np.exp(np.mean([np.log(1 + data[name][1]) for name in data])) - 1,
-        np.exp(np.mean([np.log(1 + data[name][2]) for name in data])) - 1,
+        np.exp(np.mean([np.log(1 + data[name][0]) for name in data])) - 1,
         np.exp(np.mean([np.log(1 + data[name][3]) for name in data])) - 1,
-
+        np.exp(np.mean([np.log(1 + data[name][2]) for name in data])) - 1,
     ))
+
+    print(r'\bottomrule')
 
 if __name__ == '__main__':
     main()
